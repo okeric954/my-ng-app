1539cc509461d3db36d1274a643b1d2714692836
Eric <165939664+okeric954@users.noreply.github.com>
2024-04-11 19:28:57 +0000

    update for how program starts

--- index.ts
+++ index.ts
@@ -1,11 +1,1 @@
-import express from "express";
-const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello CodeSandbox!");
-});
-
-app.listen(port, () => {
-  console.log(`Sandbox listening on port ${port}`);
-});
